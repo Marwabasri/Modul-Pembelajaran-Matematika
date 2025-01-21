d9e38a9ba3615339daf0279f3fedfbaefaf284ac
Marwabasri <wawa61926@gmail.com>
2025-01-22 07:15:58 +0800

    ubah posisi script.js ke dalam conten

--- src/conten/script.js
+++ src/conten/script.js
@@ -50,4 +50,31 @@ document.querySelectorAll('details').forEach((detail) => {
     interactiveCard.style.display = 'none';
   });
 
+    
+
+//Slider  
   
+    const sliderItems = document.querySelectorAll('[data-carousel-item]');
+    const prevButton = document.querySelector('[data-carousel-prev]');
+    const nextButton = document.querySelector('[data-carousel-next]');
+
+    let currentIndex = 0;
+
+    function updateCarousel(index) {
+      sliderItems.forEach((item, i) => {
+        item.classList.toggle('block', i === index);
+        item.classList.toggle('hidden', i !== index);
+      });
+    }
+
+    prevButton.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + sliderItems.length) % sliderItems.length;
+      updateCarousel(currentIndex);
+    });
+
+    nextButton.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % sliderItems.length;
+      updateCarousel(currentIndex);
+    });
+
+   
