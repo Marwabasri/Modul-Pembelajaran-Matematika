ce35a502fba185badee81877db0f2c76eef100d7
Marwabasri <wawa61926@gmail.com>
2025-01-27 18:23:39 +0800

    revisi index

--- src/conten/script.js
+++ src/conten/script.js
@@ -37,38 +37,6 @@ document.querySelectorAll('details').forEach((detail) => {
 
     
 
-//Slider  
-  
-const sliderItems = document.querySelectorAll('[data-carousel-item]');
-const prevButton = document.querySelector('[data-carousel-prev]');
-const nextButton = document.querySelector('[data-carousel-next]');
-
-// Periksa apakah elemen ditemukan
-if (sliderItems.length === 0 || !prevButton || !nextButton) {
-  console.error('Elemen slider tidak ditemukan.');
-} else {
-  let currentIndex = 0;
-
-  function updateCarousel(index) {
-    sliderItems.forEach((item, i) => {
-      item.classList.toggle('block', i === index);
-      item.classList.toggle('hidden', i !== index);
-    });
-  }
-
-  prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + sliderItems.length) % sliderItems.length;
-    updateCarousel(currentIndex);
-  });
-
-  nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % sliderItems.length;
-    updateCarousel(currentIndex);
-  });
-
-  // Inisialisasi slider pertama
-  updateCarousel(currentIndex);
-}
 
 
    
